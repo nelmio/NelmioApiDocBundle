3a7bf7ce414f5b819fe12e372e9324c0f790de64
Jordi Boggiano <j.boggiano@seld.be>
2012-04-20 10:09:12 +0200

    Clean up autolaoder for tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,28 +6,8 @@ function includeIfExists($file) {
     }
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../../../../../.composer/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../vendor/.composer/autoload.php'))) {
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/.composer/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../../../.composer/autoload.php'))) {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
 }
-
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'Nelmio\ApiDocBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-AnnotationRegistry::registerLoader(function($class) {
-    if (strpos($class, 'Nelmio\ApiDocBundle\Annotation\\') === 0) {
-        $path = __DIR__.'/../'.str_replace('\\', '/', substr($class, strlen('Nelmio\ApiDocBundle\\')))   .'.php';
-        require_once $path;
-    }
-    return class_exists($class, false);
-});
