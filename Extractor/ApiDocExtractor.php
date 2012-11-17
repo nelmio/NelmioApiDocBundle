1d721838569310531ae90c8dc3ea7980e225fe70
William DURAND <william.durand1@gmail.com>
2012-11-17 17:53:45 +0100

    Fix CS

--- Extractor/ApiDocExtractor.php
+++ Extractor/ApiDocExtractor.php
@@ -353,7 +353,7 @@ class ApiDocExtractor
                         'description'   => $annot->description,
                     ));
                 }
-            } else if (is_a($annot, self::FOS_REST_REQUEST_PARAM_CLASS)) {
+            } elseif (is_a($annot, self::FOS_REST_REQUEST_PARAM_CLASS)) {
                 $annotation->addParameter($annot->name, array(
                     'required'    => !$annot->nullable,
                     'dataType'    => $annot->requirements,
