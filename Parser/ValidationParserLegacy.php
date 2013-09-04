4b06a73970963e04e402daea90945d83fd8eb431
Anton S <russiancss@gmail.com>
2013-09-04 06:44:13 +0000

    ValidationParserLegacy now extends ValidationParser to avoid code duplication

--- Parser/ValidationParserLegacy.php
+++ Parser/ValidationParserLegacy.php
@@ -17,7 +17,7 @@ use Symfony\Component\Validator\Constraint;
 /**
  * Uses the Symfony Validation component to extract information about API objects. This is a backwards-compatible Validation component for Symfony2.1
  */
-class ValidationParserLegacy implements ParserInterface, PostParserInterface
+class ValidationParserLegacy extends ValidationParser implements ParserInterface, PostParserInterface 
 {
     /**
      * @var \Symfony\Component\Validator\Mapping\ClassMetadataFactoryInterface
@@ -84,97 +84,5 @@ class ValidationParserLegacy implements ParserInterface, PostParserInterface
         return $params;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function postParse(array $input, array $parameters)
-    {
-        foreach($parameters as $param => $data) {
-            if(isset($data['class']) && isset($data['children'])) {
-                $input = array('class' => $data['class']);
-                $parameters[$param]['children'] = array_merge(
-                    $parameters[$param]['children'], $this->postParse($input, $parameters[$param]['children'])
-                );
-                $parameters[$param]['children'] = array_merge(
-                    $parameters[$param]['children'], $this->parse($input, $parameters[$param]['children'])
-                );
-            }
-        }
 
-        return $parameters;
-    }
-
-    /**
-     * Create a valid documentation parameter based on an individual validation Constraint.
-     * Currently supports:
-     *  - NotBlank/NotNull
-     *  - Type
-     *  - Email
-     *  - Url
-     *  - Ip
-     *  - Length (min and max)
-     *  - Choice (single and multiple, min and max)
-     *  - Regex (match and non-match)
-     *
-     * @param Constraint $constraint The constraint metadata object.
-     * @param array $vparams         The existing validation parameters.
-     * @return mixed                 The parsed list of validation parameters.
-     */
-    protected function parseConstraint(Constraint $constraint, $vparams)
-    {
-        $class = substr(get_class($constraint), strlen('Symfony\\Component\\Validator\\Constraints\\'));
-
-        switch($class) {
-            case 'NotBlank':
-            case 'NotNull':
-                $vparams['required'] = true;
-                break;
-            case 'Type':
-                $vparams['dataType'] = $constraint->type;
-                break;
-            case 'Email':
-                $vparams['format'][] = '{email address}';
-                break;
-            case 'Url':
-                $vparams['format'][] = '{url}';
-                break;
-            case 'Ip':
-                $vparams['format'][] = '{ip address}';
-                break;
-            case 'Length':
-                $messages = array();
-                if(isset($constraint->min)) {
-                    $messages[] = "min: {$constraint->min}";
-                }
-                if(isset($constraint->max)) {
-                    $messages[] = "max: {$constraint->max}";
-                }
-                $vparams['format'][] = '{length: ' . join(', ', $messages) . '}';
-                break;
-            case 'Choice':
-                $format = '[' . join('|', $constraint->choices) . ']';
-                if($constraint->multiple) {
-                    $messages = array();
-                    if(isset($constraint->min)) {
-                        $messages[] = "min: {$constraint->min} ";
-                    }
-                    if(isset($constraint->max)) {
-                        $messages[] = "max: {$constraint->max} ";
-                    }
-                    $vparams['format'][] = '{' . join ('', $messages) . 'choice of ' . $format . '}';
-                } else {
-                    $vparams['format'][] = $format;
-                }
-                break;
-            case 'Regex':
-                if($constraint->match) {
-                    $vparams['format'][] = '{match: ' . $constraint->pattern . '}';
-                } else {
-                    $vparams['format'][] = '{not match: ' . $constraint->pattern . '}';
-                }
-                break;
-        }
-
-        return $vparams;
-    }
 }
