d4fe44bec9ce718718bf27d35abf0c1e0d742481
bdm-benzor <benzor@free.fr>
2013-09-25 18:52:38 +0200

    Update FormTypeParser.php

--- Parser/FormTypeParser.php
+++ Parser/FormTypeParser.php
@@ -91,7 +91,7 @@ class FormTypeParser implements ParserInterface
         foreach ($form as $name => $child) {
             $config = $child->getConfig();
 
-            if ($prefix != null) {
+            if ($prefix !== null) {
                 $name = sprintf('%s[%s]', $prefix, $name);
             }
 
