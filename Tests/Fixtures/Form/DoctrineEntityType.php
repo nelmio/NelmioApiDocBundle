bc2bc3653c572e5cc63ddce807f16b772a57607e
leberhome <leberknecht@bohammelnet.de>
2014-07-13 04:38:28 +0200

    - removed copyright annotation..dunno if this is correct

--- Tests/Fixtures/Form/DoctrineEntityType.php
+++ Tests/Fixtures/Form/DoctrineEntityType.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * This file is part of the NelmioApiDocBundle.
- *
- * (c) Nelmio <hello@nelm.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 namespace Nelmio\ApiDocBundle\Tests\Fixtures\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
