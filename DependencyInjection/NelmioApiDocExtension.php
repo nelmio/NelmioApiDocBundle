646017492d10ba2d35cfc3e01e77678754de693d
Bez Hermoso <bez@activelamp.com>
2014-08-07 12:06:04 -0700

    Alternate model naming strategy.

--- DependencyInjection/NelmioApiDocExtension.php
+++ DependencyInjection/NelmioApiDocExtension.php
@@ -63,6 +63,7 @@ class NelmioApiDocExtension extends Extension
         $container->setParameter('nelmio_api_doc.swagger.swagger_version', $config['swagger']['swagger_version']);
         $container->setParameter('nelmio_api_doc.swagger.api_version', $config['swagger']['api_version']);
         $container->setParameter('nelmio_api_doc.swagger.info', $config['swagger']['info']);
+        $container->setParameter('nelmio_api_doc.swagger.model_naming_strategy', $config['swagger']['model_naming_strategy']);
 
         if ($config['cache']['enabled'] === true) {
             $arguments = $container->getDefinition('nelmio_api_doc.extractor.api_doc_extractor')->getArguments();
