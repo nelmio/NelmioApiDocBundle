1f4c591fc91eb33765e4bbb1e5adbe6b99032c58
Geoffrey Vincent <g.vincent@soqrate.com>
2015-09-25 14:52:01 +0200

    Add support for expressions from security's annotations

--- Extractor/Handler/SensioFrameworkExtraHandler.php
+++ Extractor/Handler/SensioFrameworkExtraHandler.php
@@ -26,7 +26,37 @@ class SensioFrameworkExtraHandler implements HandlerInterface
                 $annotation->setCache($annot->getMaxAge());
             } elseif ($annot instanceof Security) {
                 $annotation->setAuthentication(true);
+
+                // expression
+                $expression = $this->handleExpression($annot->getExpression());
+                $annotation->setAuthenticationExpression($expression);
             }
         }
+    }
+
+    /**
+     * Handle SensioFrameworkExtraBundle security expression in order to return a more human-readable string.
+     *
+     * @param  mixed  $expression
+     * @return string
+     */
+    private function handleExpression($expression)
+    {
+        // has_role
+        $expression = preg_replace('/has_role\\((\\s*)((\'|\")(\\w+)(\'|\"))(\\s*)\\)/', '$2', $expression);
+
+        // is_granted
+        $expression = preg_replace_callback(
+            '/is_granted\\((\\s*)(\\[?)(\\s*)((\'|\")(.+)(\'|\"))(\\s*)(\\]?)(\\s*)(,(\\s*)(\\w+))?\\)/',
+            function ($matches) {
+                $attributes = 'permission '.implode(' or ', str_getcsv($matches[4]));
+                $object = (isset($matches[13])) ? ' on '.$matches[13] : '';
+
+                return $attributes.$object;
+            },
+            $expression
+        );
+
+        return (string) $expression;
     }
 }
