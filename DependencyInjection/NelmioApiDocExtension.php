c880e6949bcf256ef3b14c1e7086faa209802311
adsc <a.schweizer@cloudtec.ch>
2016-06-20 14:03:44 +0000

    Added missing parameter in the Extension

--- DependencyInjection/NelmioApiDocExtension.php
+++ DependencyInjection/NelmioApiDocExtension.php
@@ -30,6 +30,7 @@ class NelmioApiDocExtension extends Extension
         $config = $processor->processConfiguration($configuration, $configs);
 
         $container->setParameter('nelmio_api_doc.motd.template', $config['motd']['template']);
+        $container->setParameter('nelmio_api_doc.layout.template', $config['layout']['template']);
         $container->setParameter('nelmio_api_doc.exclude_sections', $config['exclude_sections']);
         $container->setParameter('nelmio_api_doc.default_sections_opened', $config['default_sections_opened']);
         $container->setParameter('nelmio_api_doc.api_name', $config['name']);
