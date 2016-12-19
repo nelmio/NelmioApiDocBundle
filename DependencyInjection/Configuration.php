2ece29cfce40bbb424957cef3b098567cd517cc4
Jose Luis Poveda <jlpoveda@gmail.com>
2016-12-19 15:54:33 +0100

    HMAC authentication support in the sandbox

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,161 +9,240 @@
  * file that was distributed with this source code.
  */
 
-namespace Nelmio\ApiDocBundle\DependencyInjection;
+namespace Nelmio\ApiDocBundle\Formatter;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Templating\EngineInterface;
 
-class Configuration implements ConfigurationInterface
+class HtmlFormatter extends AbstractFormatter
 {
-    public function getConfigTreeBuilder()
+    /**
+     * @var string
+     */
+    protected $apiName;
+
+    /**
+     * @var string
+     */
+    protected $endpoint;
+
+    /**
+     * @var string
+     */
+    protected $defaultRequestFormat;
+
+    /**
+     * @var EngineInterface
+     */
+    protected $engine;
+
+    /**
+     * @var boolean
+     */
+    private $enableSandbox;
+
+    /**
+     * @var array
+     */
+    private $requestFormats;
+
+    /**
+     * @var string
+     */
+    private $requestFormatMethod;
+
+    /**
+     * @var string
+     */
+    private $acceptType;
+
+    /**
+     * @var array
+     */
+    private $bodyFormats;
+
+    /**
+     * @var string
+     */
+    private $defaultBodyFormat;
+
+    /**
+     * @var array
+     */
+    private $authentication;
+
+    /**
+     * @var string
+     */
+    private $motdTemplate;
+
+    /**
+     * @var boolean
+     */
+    private $defaultSectionsOpened;
+
+    /**
+     * @param array $authentication
+     */
+    public function setAuthentication(array $authentication = null)
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder
-            ->root('nelmio_api_doc')
-            ->children()
-                ->scalarNode('name')->defaultValue('API documentation')->end()
-                ->arrayNode('exclude_sections')
-                    ->prototype('scalar')
-                    ->end()
-                ->end()
-                ->booleanNode('default_sections_opened')->defaultTrue()->end()
-                ->arrayNode('motd')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('template')->defaultValue('NelmioApiDocBundle::Components/motd.html.twig')->end()
-                    ->end()
-                ->end()
-                ->arrayNode('request_listener')
-                    ->beforeNormalization()
-                        ->ifTrue(function ($a) { return is_bool($a); })
-                        ->then(function ($a) { return array('enabled' => $a); })
-                    ->end()
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->booleanNode('enabled')->defaultTrue()->end()
-                        ->scalarNode('parameter')->defaultValue('_doc')->end()
-                    ->end()
-                ->end()
-                ->arrayNode('sandbox')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('enabled')->defaultTrue()->end()
-                        ->scalarNode('endpoint')->defaultNull()->end()
-                        ->scalarNode('accept_type')->defaultNull()->end()
-                        ->arrayNode('body_format')
-                            ->addDefaultsIfNotSet()
-                            ->beforeNormalization()
-                                ->ifString()
-                                ->then(function ($v) { return array('default_format' => $v); })
-                            ->end()
-                            ->children()
-                                ->arrayNode('formats')
-                                    ->defaultValue(array('form', 'json'))
-                                    ->prototype('scalar')->end()
-                                ->end()
-                                ->enumNode('default_format')
-                                    ->values(array('form', 'json'))
-                                    ->defaultValue('form')
-                                ->end()
-                            ->end()
-                        ->end()
-                        ->arrayNode('request_format')
-                            ->addDefaultsIfNotSet()
-                            ->children()
-                                ->arrayNode('formats')
-                                    ->defaultValue(array(
-                                        'json' => 'application/json',
-                                        'xml' => 'application/xml'
-                                    ))
-                                    ->prototype('scalar')->end()
-                                ->end()
-                                ->enumNode('method')
-                                    ->values(array('format_param', 'accept_header'))
-                                    ->defaultValue('format_param')
-                                ->end()
-                                ->scalarNode('default_format')->defaultValue('json')->end()
-                            ->end()
-                        ->end()
-                        ->arrayNode('authentication')
-                            ->children()
-                                ->scalarNode('delivery')
-                                    ->isRequired()
-                                    ->validate()
-                                        ->ifNotInArray(array('query', 'http', 'header'))
-                                        ->thenInvalid("Unknown authentication delivery type '%s'.")
-                                    ->end()
-                                ->end()
-                                ->scalarNode('name')->isRequired()->end()
-                                ->enumNode('type')
-                                    ->info('Required if http delivery is selected.')
-                                    ->values(array('basic', 'bearer'))
-                                ->end()
-                                ->booleanNode('custom_endpoint')->defaultFalse()->end()
-                            ->end()
-                            ->validate()
-                                ->ifTrue(function ($v) {
-                                    return 'http' === $v['delivery'] && !$v['type'] ;
-                                })
-                                ->thenInvalid('"type" is required when using http delivery.')
-                            ->end()
-                            # http_basic BC
-                            ->beforeNormalization()
-                                ->ifTrue(function ($v) {
-                                    return 'http_basic' === $v['delivery'];
-                                })
-                                ->then(function ($v) {
-                                    $v['delivery'] = 'http';
-                                    $v['type'] = 'basic';
+        $this->authentication = $authentication;
+    }
 
-                                    return $v;
-                                })
-                            ->end()
-                            ->beforeNormalization()
-                                ->ifTrue(function ($v) {
-                                    return 'http' === $v['delivery'];
-                                })
-                                ->then(function ($v) {
-                                    if ('http' === $v['delivery'] && !isset($v['name'])) {
-                                        $v['name'] = 'Authorization';
-                                    }
+    /**
+     * @param string $apiName
+     */
+    public function setApiName($apiName)
+    {
+        $this->apiName = $apiName;
+    }
 
-                                    return $v;
-                                })
-                            ->end()
-                        ->end()
-                        ->booleanNode('entity_to_choice')->defaultTrue()->end()
-                    ->end()
-                ->end()
-                ->arrayNode('swagger')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('model_naming_strategy')->defaultValue('dot_notation')->end()
-                        ->scalarNode('api_base_path')->defaultValue('/api')->end()
-                        ->scalarNode('swagger_version')->defaultValue('1.2')->end()
-                        ->scalarNode('api_version')->defaultValue('0.1')->end()
-                        ->arrayNode('info')
-                            ->addDefaultsIfNotSet()
-                                ->children()
-                                    ->scalarNode('title')->defaultValue('Symfony2')->end()
-                                    ->scalarNode('description')->defaultValue('My awesome Symfony2 app!')->end()
-                                    ->scalarNode('TermsOfServiceUrl')->defaultNull()->end()
-                                    ->scalarNode('contact')->defaultNull()->end()
-                                    ->scalarNode('license')->defaultNull()->end()
-                                    ->scalarNode('licenseUrl')->defaultNull()->end()
-                                ->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('cache')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('file')->defaultValue('%kernel.cache_dir%/api-doc.cache')->end()
-                    ->end()
-                ->end()
-            ->end();
+    /**
+     * @param string $endpoint
+     */
+    public function setEndpoint($endpoint)
+    {
+        $this->endpoint = $endpoint;
+    }
 
-        return $treeBuilder;
+    /**
+     * @param boolean $enableSandbox
+     */
+    public function setEnableSandbox($enableSandbox)
+    {
+        $this->enableSandbox = $enableSandbox;
+    }
+
+    /**
+     * @param EngineInterface $engine
+     */
+    public function setTemplatingEngine(EngineInterface $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    /**
+     * @param string $acceptType
+     */
+    public function setAcceptType($acceptType)
+    {
+        $this->acceptType = $acceptType;
+    }
+
+    /**
+     * @param array $bodyFormats
+     */
+    public function setBodyFormats(array $bodyFormats)
+    {
+        $this->bodyFormats = $bodyFormats;
+    }
+
+    /**
+     * @param string $defaultBodyFormat
+     */
+    public function setDefaultBodyFormat($defaultBodyFormat)
+    {
+        $this->defaultBodyFormat = $defaultBodyFormat;
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setRequestFormatMethod($method)
+    {
+        $this->requestFormatMethod = $method;
+    }
+
+    /**
+     * @param array $formats
+     */
+    public function setRequestFormats(array $formats)
+    {
+        $this->requestFormats = $formats;
+    }
+
+    /**
+     * @param string $format
+     */
+    public function setDefaultRequestFormat($format)
+    {
+        $this->defaultRequestFormat = $format;
+    }
+
+    /**
+     * @param string $motdTemplate
+     */
+    public function setMotdTemplate($motdTemplate)
+    {
+        $this->motdTemplate = $motdTemplate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMotdTemplate()
+    {
+        return $this->motdTemplate;
+    }
+
+    /**
+     * @param boolean $defaultSectionsOpened
+     */
+    public function setDefaultSectionsOpened($defaultSectionsOpened)
+    {
+        $this->defaultSectionsOpened = $defaultSectionsOpened;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function renderOne(array $data)
+    {
+        return $this->engine->render('NelmioApiDocBundle::resource.html.twig', array_merge(
+            array(
+                'data'           => $data,
+                'displayContent' => true,
+            ),
+            $this->getGlobalVars()
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function render(array $collection)
+    {
+        return $this->engine->render('NelmioApiDocBundle::resources.html.twig', array_merge(
+            array(
+                'resources' => $collection,
+            ),
+            $this->getGlobalVars()
+        ));
+    }
+
+    /**
+     * @return array
+     */
+    private function getGlobalVars()
+    {
+        return array(
+            'apiName'               => $this->apiName,
+            'authentication'        => $this->authentication,
+            'endpoint'              => $this->endpoint,
+            'enableSandbox'         => $this->enableSandbox,
+            'requestFormatMethod'   => $this->requestFormatMethod,
+            'acceptType'            => $this->acceptType,
+            'bodyFormats'           => $this->bodyFormats,
+            'defaultBodyFormat'     => $this->defaultBodyFormat,
+            'requestFormats'        => $this->requestFormats,
+            'defaultRequestFormat'  => $this->defaultRequestFormat,
+            'date'                  => date(DATE_RFC822),
+            'css'                   => file_get_contents(__DIR__ . '/../Resources/public/css/screen.css'),
+            'js'                    =>
+                file_get_contents(__DIR__ . '/../Resources/public/js/all.js') .
+                file_get_contents(__DIR__ . '/../Resources/public/js/hmac-sha256.js') .
+                file_get_contents(__DIR__ . '/../Resources/public/js/enc-base64-min.js'),
+            'motdTemplate'          => $this->motdTemplate,
+            'defaultSectionsOpened' => $this->defaultSectionsOpened,
+        );
     }
 }
