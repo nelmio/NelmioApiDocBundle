10f27b2c3d8e15f8f382052a9f2bae098f19e63d
gam6itko <gam6itko@gmail.com>
2017-09-17 21:59:57 +0300

    Configuration tests

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -29,12 +29,50 @@ final class Configuration implements ConfigurationInterface
                     ->prototype('variable')->end()
                 ->end()
                 ->arrayNode('routes')
+                    ->beforeNormalization()
+                        ->ifTrue(function ($v) {
+                            return is_array($v) && array_key_exists('path_patterns', $v) && is_array($v['path_patterns']) && !array_key_exists('path_patterns', $v['path_patterns']);
+                        })
+                        ->then(function ($v) {
+                            $v['host'] = null;
+                            return [$v];
+                        })
+                    ->end()
+                    ->beforeNormalization()
+                        ->ifTrue(function ($v) { // make sure that each host was mentioned once
+                            $hosts = [];
+                            foreach ($v as $arr) {
+                                if (in_array($arr['host'], $hosts)) {
+                                    return true;
+                                }
+                                $hosts[] = $arr['host'];
+                            }
+                            return false;
+                        })
+                        ->then(function ($v) {
+                            $result = [];
+                            foreach ($v as $arr){
+                                foreach ($result as &$item) {
+                                    if ($arr['host'] === $item['host']) {
+                                        $item['path_patterns'] = array_unique(array_merge($item['path_patterns'], $arr['path_patterns']));
+                                        continue 2;
+                                    }
+                                }
+                                $result[] = $arr;
+                            }
+                            return $result;
+                        })
+                    ->end()
                     ->info('Filter the routes that are documented')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->arrayNode('path_patterns')
-                            ->example(['^/api', '^/api(?!/admin)'])
-                            ->prototype('scalar')->end()
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('host')
+                                ->defaultValue(null)
+                            ->end()
+                            ->arrayNode('path_patterns')
+                                ->example(['^/api', '^/api(?!/admin)'])
+                                ->prototype('scalar')->end()
+                            ->end()
                         ->end()
                     ->end()
                 ->end()
--- Tests/Routing/FilteredRouteCollectionBuilderTest.php
+++ Tests/Routing/FilteredRouteCollectionBuilderTest.php
@@ -13,6 +13,8 @@ namespace Nelmio\ApiDocBundle\Tests\Routing;
 
 use Nelmio\ApiDocBundle\Routing\FilteredRouteCollectionBuilder;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -21,13 +23,33 @@ use Symfony\Component\Routing\RouteCollection;
  */
 class FilteredRouteCollectionBuilderTest extends TestCase
 {
-    public function testFilter()
+    /**
+     * @dataProvider routesProvider
+     * @param RequestStack $requestStack
+     * @param RouteCollection $routes
+     * @param $expectedCount
+     */
+    public function testFilter($requestStack, $routes, $expectedCount)
     {
-        $pathPattern = [
-            '^/api/foo',
-            '^/api/bar',
+        $routesConfig = [
+            ['host' => null, 'path_patterns' => ['^/api/foo', '^/api/bar']],
+            ['host' => 'fakehost2.com', 'path_patterns' => ['^/api/demo']]
         ];
 
+        $routeBuilder = new FilteredRouteCollectionBuilder($requestStack, $routesConfig);
+        $filteredRoutes = $routeBuilder->filter($routes);
+
+        $this->assertCount($expectedCount, $filteredRoutes);
+    }
+
+    public function routesProvider()
+    {
+        $result = [];
+
+        $requestStack = new RequestStack();
+        $fakeRequest = Request::create('/', 'GET');
+        $fakeRequest->headers->add(['HOST' => 'fakehost.com']);
+        $requestStack->push($fakeRequest);
         $routes = new RouteCollection();
         $routes->add('r1', new Route('/api/bar/action1'));
         $routes->add('r2', new Route('/api/foo/action1'));
@@ -35,9 +57,34 @@ class FilteredRouteCollectionBuilderTest extends TestCase
         $routes->add('r4', new Route('/api/demo'));
         $routes->add('r5', new Route('/_profiler/test/test'));
 
-        $routeBuilder = new FilteredRouteCollectionBuilder($pathPattern);
-        $filteredRoutes = $routeBuilder->filter($routes);
+        $result[] = [$requestStack, $routes, 3];
 
-        $this->assertCount(3, $filteredRoutes);
+        $requestStack = new RequestStack();
+        $fakeRequest = Request::create('/', 'GET');
+        $fakeRequest->headers->add(['HOST' => 'fakehost2.com']);
+        $requestStack->push($fakeRequest);
+        $routes = new RouteCollection();
+        $routes->add('r1', new Route('/api/bar/action1'));
+        $routes->add('r2', new Route('/api/foo/action1'));
+        $routes->add('r3', new Route('/api/foo/action2', [], [], [], 'fakehost2.com'));
+        $routes->add('r4', new Route('/api/demo', [], [], [], 'fakehost2.com'));
+        $routes->add('r5', new Route('/_profiler/test/test'));
+
+        $result[] = [$requestStack, $routes, 4];
+
+
+        $requestStack = new RequestStack();
+        $fakeRequest = Request::create('/', 'GET');
+        $fakeRequest->headers->add(['HOST' => 'fakehost2.com']);
+        $requestStack->push($fakeRequest);
+        $routes = new RouteCollection();
+        $routes->add('r1', new Route('/api/baz/action1'));
+        $routes->add('r2', new Route('/api/you/shall/not/pass'));
+        $routes->add('r4', new Route('/api/demo', [], [], [], 'fakehost2.com'));
+        $routes->add('r5', new Route('/_profiler/test/test'));
+
+        $result[] = [$requestStack, $routes, 1];
+
+        return $result;
     }
 }
