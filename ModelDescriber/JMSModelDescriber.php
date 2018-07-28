71e7ffde309c91f3478cb7849310e7dc761d5db9
Javier Spagnoletti <phansys@gmail.com>
2018-07-28 09:53:58 -0300

    Refactor `describeItem()`

--- ModelDescriber/JMSModelDescriber.php
+++ ModelDescriber/JMSModelDescriber.php
@@ -173,7 +173,9 @@ class JMSModelDescriber implements ModelDescriberInterface, ModelRegistryAwareIn
                 // in the case of a virtual property, set it as free object type
                 $property->merge(['additionalProperties' => []]);
 
-                $this->describeItem($nestedType, $property->getAdditionalProperties(), $groups);
+                if ('array' !== $nestedType['name']) {
+                    $this->describeItem($nestedType, $property->getAdditionalProperties(), $groups);
+                }
 
                 return;
             }
@@ -183,10 +185,6 @@ class JMSModelDescriber implements ModelDescriberInterface, ModelRegistryAwareIn
         }
 
         if ($typeDef = $this->findPropertyType($type['name'], $groups)) {
-            if (isset($typeDef['type']) && 'array' === $typeDef['type'] && null === $property->getItems()->toArray()) {
-                unset($typeDef['type']);
-            }
-
             $this->registerPropertyType($typeDef, $property);
         }
     }
