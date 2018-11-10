a1e1c6b725a51bb716dbde04b1b24d06aef7f2c8
neFAST <neFAST@users.noreply.github.com>
2018-11-10 13:41:38 +0100

    Update UserType.php

--- Tests/Functional/Form/UserType.php
+++ Tests/Functional/Form/UserType.php
@@ -14,8 +14,8 @@ namespace Nelmio\ApiDocBundle\Tests\Functional\Form;
 use Nelmio\ApiDocBundle\Tests\Functional\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
