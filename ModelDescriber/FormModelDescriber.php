8d0d449b33a1a41c98b4513c7210d8ad4af22459
neFAST <neFAST@users.noreply.github.com>
2018-11-10 13:53:02 +0100

    removed check to see how unit test handles this

--- ModelDescriber/FormModelDescriber.php
+++ ModelDescriber/FormModelDescriber.php
@@ -75,9 +75,7 @@ final class FormModelDescriber implements ModelDescriberInterface, ModelRegistry
                 $schema->setRequired($required);
             }
 
-            if ($config->hasOption('documentation')) {
-                $property->merge($config->getOption('documentation'));
-            }
+            $property->merge($config->getOption('documentation'));
             if (null !== $property->getType()) {
                 continue; // Type manually defined
             }
