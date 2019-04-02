38d805234ec5902f1bd97de63c57c9f392f6736c
Ayrton Ricardo <ayrtonricardo.dasilva@check24.de>
2019-04-02 15:56:32 +0200

    Revert not needed update on expected result

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -214,7 +214,7 @@ class FunctionalTest extends WebTestCase
                         '$ref' => '#/definitions/User',
                     ],
                     'dummy' => [
-                        '$ref' => '#/definitions/Dummy',
+                        '$ref' => '#/definitions/Dummy2',
                     ],
                     'status' => [
                         'type' => 'string',
