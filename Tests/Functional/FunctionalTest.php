6cb54d191ccc2bff59a03812e48dcd8c1a47050f
Filip Benčo <filip.benco@websupport.sk>
2020-06-16 13:03:08 +0200

    Revert "Revert "Another CS""
    This reverts commit a5b08dedf5bca8fb711b816c62bed2de9f1c9521.

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -192,8 +192,8 @@ class FunctionalTest extends WebTestCase
                     'friend' => [
                         'nullable' => true,
                         'anyOf' => [
-                            ['$ref' => '#/components/schemas/User']
-                        ]
+                            ['$ref' => '#/components/schemas/User'],
+                        ],
                     ],
                     'dummy' => [
                         '$ref' => '#/components/schemas/Dummy2',
