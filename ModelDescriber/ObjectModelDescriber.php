bf08af144facff2b66c095a718964a6bde21c919
Guilhem Niot <guilhem.niot@gmail.com>
2020-06-18 18:30:12 +0200

    Always use the SerializerExtractor (from the PropertyInfo component)

--- ModelDescriber/ObjectModelDescriber.php
+++ ModelDescriber/ObjectModelDescriber.php
@@ -61,9 +61,9 @@ class ObjectModelDescriber implements ModelDescriberInterface, ModelRegistryAwar
         $class = $model->getType()->getClassName();
         $schema->_context->class = $class;
 
-        $context = [];
+        $context = ['serializer_groups' => null]; // Use the SerializerExtractor with not groups check (sf >= 5.1)
         if (null !== $model->getGroups()) {
-            $context = ['serializer_groups' => array_filter($model->getGroups(), 'is_string')];
+            $context['serializer_groups'] = array_filter($model->getGroups(), 'is_string');
         }
 
         $annotationsReader = new AnnotationsReader($this->doctrineReader, $this->modelRegistry, $this->mediaTypes);
