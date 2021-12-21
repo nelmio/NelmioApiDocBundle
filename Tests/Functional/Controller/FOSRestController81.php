b333892b8157f8e2a65596b933de5a80e65676aa
Guilhem Niot <guilhem@gniot.fr>
2021-12-21 14:58:36 +0100

    typo

--- Tests/Functional/Controller/FOSRestController81.php
+++ Tests/Functional/Controller/FOSRestController81.php
@@ -19,17 +19,16 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class FOSRestController81 extends FOSRestController80 {
-        #[Route('/fosrest_attributes.{_format}', methods: ['POST'])]
-        #[QueryParam(name: 'foo', requirements: new Regex('/^\d+$/'))]
-        #[QueryParam(name: 'mapped', map: true)]
-        #[RequestParam(name: 'Barraa', key: 'bar', requirements: '\d+')]
-        #[RequestParam(name: 'baz', requirements: new IsTrue())]
-        #[RequestParam(name: 'datetime', requirements: new DateTime('Y-m-d\TH:i:sP'))]
-        #[RequestParam(name: 'datetimeAlt', requirements: new DateTime('c'))]
-        #[RequestParam(name: 'datetimeNoFormat', requirements: new DateTime())]
-        #[RequestParam(name: 'date', requirements: new DateTime('Y-m-d'))]
-        public function fosrestAttributesAction()
-        {
-        }
+    #[Route('/fosrest_attributes.{_format}', methods: ['POST'])]
+    #[QueryParam(name: 'foo', requirements: new Regex('/^\d+$/'))]
+    #[QueryParam(name: 'mapped', map: true)]
+    #[RequestParam(name: 'Barraa', key: 'bar', requirements: '\d+')]
+    #[RequestParam(name: 'baz', requirements: new IsTrue())]
+    #[RequestParam(name: 'datetime', requirements: new DateTime('Y-m-d\TH:i:sP'))]
+    #[RequestParam(name: 'datetimeAlt', requirements: new DateTime('c'))]
+    #[RequestParam(name: 'datetimeNoFormat', requirements: new DateTime())]
+    #[RequestParam(name: 'date', requirements: new DateTime('Y-m-d'))]
+    public function fosrestAttributesAction()
+    {
     }
 }
