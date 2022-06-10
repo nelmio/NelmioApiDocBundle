e422e3cb11ee9c7564743c6a31f0b13127cf5eec
Guilhem Niot <guilhem@gniot.fr>
2022-06-10 22:34:23 +0200

    Add a test + Fix implementation

---
+++ Tests/Functional/Form/FormWithModel.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the NelmioApiDocBundle package.
+ *
+ * (c) Nelmio
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\ApiDocBundle\Tests\Functional\Form;
+
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Tests\Functional\Entity\User;
+use OpenApi\Annotations as OA;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormWithModel extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quz', TextType::class, ['documentation' => ['ref' => new Model(["type" => User::class])]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+    }
+}
