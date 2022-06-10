f33eee70fcdd9e0a41da01c1a6742ffde8b68d38
Guilhem Niot <guilhem@gniot.fr>
2022-06-10 23:17:15 +0200

    Catch a more precise exception in Swagger controller (#2005)

--- Controller/SwaggerUiController.php
+++ Controller/SwaggerUiController.php
@@ -11,7 +11,7 @@
 
 namespace Nelmio\ApiDocBundle\Controller;
 
-use InvalidArgumentException;
+use Nelmio\ApiDocBundle\Exception\RenderInvalidArgumentException;
 use Nelmio\ApiDocBundle\Render\Html\AssetsMode;
 use Nelmio\ApiDocBundle\Render\RenderOpenApi;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,7 +42,7 @@ final class SwaggerUiController
             );
 
             return $response->setCharset('UTF-8');
-        } catch (InvalidArgumentException $e) {
+        } catch (RenderInvalidArgumentException $e) {
             $advice = '';
             if (false !== strpos($area, '.json')) {
                 $advice = ' Since the area provided contains `.json`, the issue is likely caused by route priorities. Try switching the Swagger UI / the json documentation routes order.';
---
+++ Exception/RenderInvalidArgumentException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the NelmioApiDocBundle package.
+ *
+ * (c) Nelmio
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\ApiDocBundle\Exception;
+
+class RenderInvalidArgumentException extends \InvalidArgumentException
+{
+}
