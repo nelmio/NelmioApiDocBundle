1aa2cbd6c64b053983a4f0c4945e890af1ee4f94
Luzius Hausammann <luz80@gmx.net>
2023-01-04 20:16:31 +0100

    Add enum description to route parameter if possible. (#2062)
    * Add enum description to route parameter if possible.
    
    * code style
    
    * Use strpos instead of str_contains
    
    * Ignore dots for enum, add tests
    
    * Update docstring
    
    Co-authored-by: Guilhem Niot <guilhem@gniot.fr>

--- Tests/RouteDescriber/RouteMetadataDescriberTest.php
+++ Tests/RouteDescriber/RouteMetadataDescriberTest.php
@@ -14,6 +14,7 @@ namespace Nelmio\ApiDocBundle\Tests\RouteDescriber;
 use Nelmio\ApiDocBundle\RouteDescriber\RouteMetadataDescriber;
 use OpenApi\Annotations\OpenApi;
 use OpenApi\Context;
+use OpenApi\Generator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Routing\Route;
 
@@ -24,5 +25,90 @@ class RouteMetadataDescriberTest extends TestCase
         $routeDescriber = new RouteMetadataDescriber();
 
         $this->assertNull($routeDescriber->describe(new OpenApi(['_context' => new Context()]), new Route('foo'), new \ReflectionMethod(__CLASS__, 'testUndefinedCheck')));
+    }
+
+    public function testRouteRequirementsWithPattern()
+    {
+        $api = new OpenApi([]);
+        $routeDescriber = new RouteMetadataDescriber();
+        $route = new Route('/index/{bar}/{foo}.html', [], ['foo' => '[0-9]|[a-z]'], [], 'localhost', 'https', ['GET']);
+        $routeDescriber->describe(
+            $api,
+            $route,
+            new \ReflectionMethod(__CLASS__, 'testRouteRequirementsWithPattern')
+        );
+
+        $this->assertEquals('/index/{bar}/{foo}.html', $api->paths[0]->path);
+        $getPathParameter = $api->paths[0]->get->parameters[1];
+        if ('foo' === $getPathParameter->name) {
+            $this->assertEquals('path', $getPathParameter->in);
+            $this->assertEquals('foo', $getPathParameter->name);
+            $this->assertEquals('string', $getPathParameter->schema->type);
+            $this->assertEquals('[0-9]|[a-z]', $getPathParameter->schema->pattern);
+        }
+    }
+
+    /**
+     * @dataProvider provideEnumPattern
+     */
+    public function testSimpleOrRequirementsAreHandledAsEnums($req)
+    {
+        $api = new OpenApi([]);
+        $routeDescriber = new RouteMetadataDescriber();
+        $route = new Route('/index/{bar}/{foo}.html', [], ['foo' => $req], [], 'localhost', 'https', ['GET']);
+        $routeDescriber->describe(
+            $api,
+            $route,
+            new \ReflectionMethod(__CLASS__, 'testSimpleOrRequirementsAreHandledAsEnums')
+        );
+
+        $this->assertEquals('/index/{bar}/{foo}.html', $api->paths[0]->path);
+        $getPathParameter = $api->paths[0]->get->parameters[1];
+        $this->assertEquals('path', $getPathParameter->in);
+        $this->assertEquals('foo', $getPathParameter->name);
+        $this->assertEquals('string', $getPathParameter->schema->type);
+        $this->assertEquals(explode('|', $req), $getPathParameter->schema->enum);
+        $this->assertEquals($req, $getPathParameter->schema->pattern);
+    }
+
+    /**
+     * @dataProvider provideInvalidEnumPattern
+     */
+    public function testNonEnumPatterns($pattern)
+    {
+        $api = new OpenApi([]);
+        $routeDescriber = new RouteMetadataDescriber();
+        $route = new Route('/index/{foo}.html', [], ['foo' => $pattern], [], 'localhost', 'https', ['GET']);
+        $routeDescriber->describe(
+            $api,
+            $route,
+            new \ReflectionMethod(__CLASS__, 'testNonEnumPatterns')
+        );
+
+        $getPathParameter = $api->paths[0]->get->parameters[0];
+        $this->assertEquals($pattern, $getPathParameter->schema->pattern);
+        $this->assertEquals(Generator::UNDEFINED, $getPathParameter->schema->enum);
+    }
+
+    public function provideEnumPattern()
+    {
+        yield ['1|2|3'];
+        yield ['srf|rtr|rsi'];
+        yield ['srf-1|srf-2'];
+        yield ['srf-1|srf-2'];
+    }
+
+    public function provideInvalidEnumPattern()
+    {
+        yield ['|'];
+        yield ['|a'];
+        yield ['srf|*|rtr'];
+        yield ['srf||rtr'];
+        yield ['1|2|'];
+        yield ['/1|2/'];
+        yield ['\d|a'];
+        // dots have special meaning and should be skipped
+        yield ['a_1\.html|b-2\.html'];
+        yield ['a_1.html|b-2.html'];
     }
 }
