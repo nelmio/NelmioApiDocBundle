3c53bb1cdab389e38f599b3ed0b881dd29cb482d
DjordyKoert <djordy.koert@yoursurprise.com>
2023-08-12 00:44:36 +0200

    Use accessible values for tests

--- Tests/RouteDescriber/Fixtures/SymfonyDescriberMapQueryStringClass.php
+++ Tests/RouteDescriber/Fixtures/SymfonyDescriberMapQueryStringClass.php
@@ -10,19 +10,33 @@ use OpenApi\Annotations\Property;
 use OpenApi\Annotations\Schema;
 use Symfony\Component\PropertyInfo\Type;
 
-class SymfonyDescriberMapQueryStringClass implements SelfDescribingModelInterface
+final class SymfonyDescriberMapQueryStringClass implements SelfDescribingModelInterface
 {
+    public const SCHEMA = 'SymfonyDescriberMapQueryStringClass';
+    public const TITLE = 'SelfDescribingTitle';
+    public const TYPE = 'object';
+
     public static function describe(Schema $schema, Model $model): void
     {
-        $schema->title = 'SelfDescribingTitle';
+        $schema->schema = self::SCHEMA;
+        $schema->title = self::TITLE;
         $schema->description = $model->getType()->getClassName();
-        $schema->type = 'object';
+        $schema->type = self::TYPE;
 
-        $schema->properties = [
+        $schema->properties = self::getProperties();
+    }
+
+    /**
+     * @return Property[]
+     */
+    public static function getProperties(): array
+    {
+        return [
             new Property([
                 'property' => 'id',
                 'type' => Type::BUILTIN_TYPE_INT,
                 'nullable' => false,
+                'default' => 123,
             ]),
         ];
     }
