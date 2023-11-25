c88e4a7cdf4ec8bc27f3efac4cb41b7094dd5c6e
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-11-25 16:47:17 +0100

    Update Tests

--- Tests/ApiDocGeneratorTest.php
+++ Tests/ApiDocGeneratorTest.php
@@ -13,6 +13,7 @@ namespace Nelmio\ApiDocBundle\Tests;
 
 use Nelmio\ApiDocBundle\ApiDocGenerator;
 use Nelmio\ApiDocBundle\Describer\DefaultDescriber;
+use OpenApi\Generator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
@@ -21,7 +22,7 @@ class ApiDocGeneratorTest extends TestCase
     public function testCache()
     {
         $adapter = new ArrayAdapter();
-        $generator = new ApiDocGenerator([new DefaultDescriber()], [], $adapter);
+        $generator = new ApiDocGenerator([new DefaultDescriber()], [], new Generator(), $adapter);
 
         $this->assertEquals(json_encode($generator->generate()), json_encode($adapter->getItem('openapi_doc')->get()));
     }
@@ -29,7 +30,7 @@ class ApiDocGeneratorTest extends TestCase
     public function testCacheWithCustomId()
     {
         $adapter = new ArrayAdapter();
-        $generator = new ApiDocGenerator([new DefaultDescriber()], [], $adapter, 'custom_id');
+        $generator = new ApiDocGenerator([new DefaultDescriber()], [], new Generator(), $adapter, 'custom_id');
 
         $this->assertEquals(json_encode($generator->generate()), json_encode($adapter->getItem('custom_id')->get()));
     }
