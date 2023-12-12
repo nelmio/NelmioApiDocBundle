fdbe1e7e370013a3b2bbba846b52fe2303ae128f
DjordyKoert <djordy.koert@yoursurprise.com>
2023-12-12 01:05:49 +0100

    Re-add ArrayItemsErrorController tests

--- Tests/Functional/Controller/ArrayItemsErrorController.php
+++ Tests/Functional/Controller/ArrayItemsErrorController.php
@@ -14,24 +14,38 @@ namespace Nelmio\ApiDocBundle\Tests\Functional\Controller;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Tests\Functional\Entity\ArrayItemsError\Foo;
 use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OAT;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route(host="api.example.com")
- */
-class ArrayItemsErrorController
-{
+if (Kernel::MAJOR_VERSION < 7) {
     /**
-     * @Route("/api/error", methods={"GET"})
-     *
-     * @OA\Response(
-     *     response=200,
-     *     description="Success",
-     *
-     *     @Model(type=Foo::class)
-     * )
+     * @Route(host="api.example.com")
      */
-    public function errorAction()
+    class ArrayItemsErrorController
     {
+        /**
+         * @Route("/api/error", methods={"GET"})
+         *
+         * @OA\Response(
+         *     response=200,
+         *     description="Success",
+         *
+         *     @Model(type=Foo::class)
+         * )
+         */
+        public function errorAction()
+        {
+        }
+    }
+} else {
+    #[Route(host: 'api.example.com')]
+    class ArrayItemsErrorController
+    {
+        #[Route('/api/error', methods: ['GET'])]
+        #[OAT\Response(response: 200, description: 'Success', content: new Model(type: Foo::class))]
+        public function errorAction()
+        {
+        }
     }
 }
