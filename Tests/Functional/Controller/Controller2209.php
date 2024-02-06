c46beb88e6e3d9957847c1117b9dd8745dad9519
DjordyKoert <djordy.koert@yoursurprise.com>
2024-02-06 16:59:37 +0100

    uncomment commented code

--- Tests/Functional/Controller/Controller2209.php
+++ Tests/Functional/Controller/Controller2209.php
@@ -15,9 +15,9 @@ class Controller2209
 {
     #[Route(path: '/api/v3/users', name: 'api_v3_users_create', methods: 'POST')]
     #[OA\RequestBody(
-//        content: new OA\MediaType(mediaType: 'application/json', schema: new OA\Schema(
-//            ref: new Model(type: Article81::class),
-//        )),
+        content: new OA\MediaType(mediaType: 'application/json', schema: new OA\Schema(
+            ref: new Model(type: Article81::class),
+        )),
     )]
     public function __invoke(#[MapRequestPayload] Article81 $requestDTO): JsonResponse
     {
