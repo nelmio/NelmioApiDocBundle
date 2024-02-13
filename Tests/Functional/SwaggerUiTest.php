7bcf764333408d404fd0fa496ec1f44ebf36c595
DjordyKoert <djordy.koert@yoursurprise.com>
2024-02-13 14:48:32 +0100

    add test for redocly ui

--- Tests/Functional/SwaggerUiTest.php
+++ Tests/Functional/SwaggerUiTest.php
@@ -46,6 +46,24 @@ class SwaggerUiTest extends WebTestCase
         $this->assertEquals($expected, json_decode($crawler->filterXPath('//script[@id="swagger-data"]')->text(), true)['spec']);
     }
 
+    public function testRedocly()
+    {
+        $crawler = $this->client->request('GET', '/app_dev.php/default/redocly/docs');
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('UTF-8', $response->getCharset());
+        $this->assertEquals('text/html; charset=UTF-8', $response->headers->get('Content-Type'));
+
+        $expected = json_decode($this->getOpenApiDefinition()->toJson(), true);
+        $expected['servers'] = [
+            ['url' => 'http://api.example.com/app_dev.php'],
+        ];
+
+        $this->assertSame(1, $crawler->filterXPath('//script[@src="/bundles/nelmioapidoc/redocly/redoc.standalone.js"]')->count());
+        $this->assertEquals($expected, json_decode($crawler->filterXPath('//script[@id="swagger-data"]')->text(), true)['spec']);
+    }
+
     public function testApiPlatformSwaggerUi()
     {
         $crawler = $this->client->request('GET', '/app_dev.php/test/docs');
