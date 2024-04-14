fd1f6c1b0a7694ab47be9c18eeb66b9d3c4a4c20
lhausammann <luz80@gmx.net>
2024-04-14 19:36:21 +0200

    Add unnamed route to OperationIdController and update fixtures accordingly.

--- tests/Functional/Controller/OperationIdController.php
+++ tests/Functional/Controller/OperationIdController.php
@@ -20,14 +20,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OperationIdController
 {
-    // a route with only a symfony route annotation (generate GET with
-    #[Route(path: '/generate/operation_id_route', name: 'only_route', methods: 'GET')]
+    // a route with only a symfony route annotation (generates GET Operation using available metadata as operationId)
+    #[Route(path: '/generate/operation_id_route_unnamed', methods: 'GET')]
+    public function getMustGenerateOperationIdByUnnamedRouteAnnotation(): JsonResponse
+    {
+        return new JsonResponse();
+    }
+    // a route with a named symfony route annotation (generates GET Operation using route name as operationId)
+    #[Route(path: '/generate/operation_id_route', name: 'named_route', methods: 'GET')]
     public function getMustGenerateOperationIdByRouteAnnotation(): JsonResponse
     {
         return new JsonResponse();
     }
 
-    // a route with an OA\GET annotation
+    // a route with an OA\Get annotation and a separate ApiDoc Annotation(extends GET operation with operationId)
+    #[Route(path: '/generate/operation_id_with_security/', name: 'with_security', methods: 'GET')]
+    #[OA\Get('OperationId must be generated automatically when additional OA/nelmio root annotations are present')]
+    #[Security(name: 'bearerAuth')] // additioanl root annotations
+    public function getWithAdditionalAnnotationsGeneratesOperationId(): JsonResponse
+    {
+        return new JsonResponse();
+    }
+
+    // a route with an OA\GET annotation (extends GET operation with operationId)
     #[Route(path: '/generate/operation_id_get', name: 'get_annotation', methods: 'GET')]
     #[OA\Get(summary: 'OperationId must be generated automatically if not provided')]
     public function getMustGenerateOperationIByGetAnnotation(): JsonResponse
@@ -35,17 +50,10 @@ class OperationIdController
         return new JsonResponse();
     }
 
+   // custom operationId is uesed when set explicitly
     #[Route(path: '/has/explicit/operationid', name: 'customOperationId', methods: 'GET')]
     #[OA\Get(summary: 'Custom operation id must be taken', operationId: 'customOperationId')]
     public function getWithCustomOperationId(): JsonResponse
-    {
-        return new JsonResponse();
-    }
-
-    #[Route(path: '/generate/operation_id_with_security/', name: 'with_security', methods: 'GET')]
-    #[OA\Get('OperationId must be generated automatically when additional OA/nelmio root annotations are present')]
-    #[Security(name: 'bearerAuth')] // additioanl root annotations
-    public function getWithAdditionalAnnotationsGeneratesOperationId(): JsonResponse
     {
         return new JsonResponse();
     }
