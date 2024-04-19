92eb020b42d1ee894e0705efcae110ba0332cb8d
DjordyKoert <djordy.koert@yoursurprise.com>
2024-04-19 15:51:25 +0200

    add html_config.assets_mode configuration validation

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -13,14 +13,23 @@ namespace Nelmio\ApiDocBundle\Tests\DependencyInjection;
 
 use Nelmio\ApiDocBundle\DependencyInjection\Configuration;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\Definition\Processor;
 
 class ConfigurationTest extends TestCase
 {
+    private Processor $processor;
+
+    protected function setUp(): void
+    {
+        $this->processor = new Processor();
+
+        parent::setUp();
+    }
+
     public function testDefaultArea(): void
     {
-        $processor = new Processor();
-        $config = $processor->processConfiguration(new Configuration(), [['areas' => ['path_patterns' => ['/foo']]]]);
+        $config = $this->processor->processConfiguration(new Configuration(), [['areas' => ['path_patterns' => ['/foo']]]]);
 
         self::assertSame(
             [
@@ -39,8 +48,7 @@ class ConfigurationTest extends TestCase
 
     public function testAreas(): void
     {
-        $processor = new Processor();
-        $config = $processor->processConfiguration(new Configuration(), [['areas' => $areas = [
+        $config = $this->processor->processConfiguration(new Configuration(), [['areas' => $areas = [
             'default' => [
                 'path_patterns' => ['/foo'],
                 'host_patterns' => [],
@@ -72,8 +80,7 @@ class ConfigurationTest extends TestCase
 
     public function testAlternativeNames(): void
     {
-        $processor = new Processor();
-        $config = $processor->processConfiguration(new Configuration(), [[
+        $config = $this->processor->processConfiguration(new Configuration(), [[
             'models' => [
                 'names' => [
                     [
@@ -147,5 +154,30 @@ class ConfigurationTest extends TestCase
                 'areas' => [],
             ],
         ], $config['models']['names']);
+    }
+
+    /**
+     * @dataProvider provideInvalidConfiguration
+     *
+     * @param mixed[] $configuration
+     */
+    public function testInvalidConfiguration(array $configuration, string $expectedError): void
+    {
+        self::expectException(InvalidConfigurationException::class);
+        self::expectExceptionMessage($expectedError);
+
+        $this->processor->processConfiguration(new Configuration(), [$configuration]);
+    }
+
+    public static function provideInvalidConfiguration(): \Generator
+    {
+        yield 'invalid html_config.assets_mode' => [
+            [
+                'html_config' => [
+                    'assets_mode' => 'invalid',
+                ],
+            ],
+            'Invalid assets mode "invalid"',
+        ];
     }
 }
