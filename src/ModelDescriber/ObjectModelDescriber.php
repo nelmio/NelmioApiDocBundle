c3136495720dcd5442184f6dc384ac64b7631821
djordy <djordy.koert@yoursurprise.com>
2024-10-17 16:31:44 +0200

    Deprecate RequiredPropertyDescriber

--- src/ModelDescriber/ObjectModelDescriber.php
+++ src/ModelDescriber/ObjectModelDescriber.php
@@ -182,6 +182,8 @@ class ObjectModelDescriber implements ModelDescriberInterface, ModelRegistryAwar
 
             $this->describeProperty($types, $model, $property, $propertyName, $schema);
         }
+
+        $this->markRequiredProperties($schema);
     }
 
     /**
@@ -231,6 +233,24 @@ class ObjectModelDescriber implements ModelDescriberInterface, ModelRegistryAwar
         }
 
         throw new \Exception(sprintf('Type "%s" is not supported in %s::$%s. You may use the `@OA\Property(type="")` annotation to specify it manually.', $types[0]->getBuiltinType(), $model->getType()->getClassName(), $propertyName));
+    }
+
+    private function markRequiredProperties(OA\Schema $schema): void
+    {
+        if (Generator::isDefault($properties = $schema->properties)) {
+            return;
+        }
+
+        foreach ($properties as $property) {
+            if (true === $property->nullable || !Generator::isDefault($property->default)) {
+                continue;
+            }
+
+            $existingRequiredFields = Generator::UNDEFINED !== $schema->required ? $schema->required : [];
+            $existingRequiredFields[] = $property->property;
+
+            $schema->required = array_values(array_unique($existingRequiredFields));
+        }
     }
 
     public function supports(Model $model): bool
