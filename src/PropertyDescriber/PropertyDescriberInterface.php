nelmio/NelmioApiDocBundle

commit f46437ae2c3a7ce1f0b60809d058ebbc1f81cb9b

refactor: determining property required logic  (#2359)
| Q | A |

|---------------|---------------------------------------------------------------------------------------------------------------------------|
| Bug fix? | yes |
| New feature? | no |
| Deprecations? |no                                                  |

Move the behaviour for determining required property.

phpstan-baseline.neon
@@ -36,11 +36,6 @@ parameters:
 			path: src/PropertyDescriber/PropertyDescriberInterface.php
 
 		-
-			message: "#^PHPDoc tag @param references unknown parameter\\: \\$schema$#"
-			count: 1
-			path: src/PropertyDescriber/PropertyDescriberInterface.php
-
-		-
 			message: "#^Method Nelmio\\\\ApiDocBundle\\\\PropertyDescriber\\\\PropertyDescriberInterface\\:\\:describe\\(\\) invoked with 5 parameters, 2\\-3 required\\.$#"
 			count: 1
 			path: src/PropertyDescriber/RequiredPropertyDescriber.php

src/PropertyDescriber/PropertyDescriberInterface.php
@@ -19,12 +19,11 @@ interface PropertyDescriberInterface
     /**
      * @param Type[]               $types
      * @param string[]|null        $groups  Deprecated use $context['groups'] instead
-     * @param Schema               $schema  Allows to make changes inside of the schema (e.g. adding required fields)
      * @param array<string, mixed> $context Context options for describing the property
      *
      * @return void
      */
-    public function describe(array $types, Schema $property, ?array $groups = null /* , ?Schema $schema = null */ /* , array $context = [] */);
+    public function describe(array $types, Schema $property, ?array $groups = null /* , array $context = [] */);
 
     /**
      * @param Type[] $types

config/services.xml
@@ -139,10 +139,6 @@
             <tag name="nelmio_api_doc.object_model.property_describer" />
         </service>
 
-        <service id="nelmio_api_doc.object_model.property_describers.required" class="Nelmio\ApiDocBundle\PropertyDescriber\RequiredPropertyDescriber" public="false">
-            <tag name="nelmio_api_doc.object_model.property_describer" />
-        </service>
-
         <service id="nelmio_api_doc.object_model.property_describers.object" class="Nelmio\ApiDocBundle\PropertyDescriber\ObjectPropertyDescriber" public="false">
             <tag name="nelmio_api_doc.object_model.property_describer" priority="-1000" />
         </service>

src/ModelDescriber/ObjectModelDescriber.php
@@ -182,6 +182,8 @@ class ObjectModelDescriber implements ModelDescriberInterface, ModelRegistryAwar
 
             $this->describeProperty($types, $model, $property, $propertyName, $schema);
         }
+
+        $this->markRequiredProperties($schema);
     }
 
     /**
@@ -231,6 +233,36 @@ class ObjectModelDescriber implements ModelDescriberInterface, ModelRegistryAwar
         }
 
         throw new \Exception(sprintf('Type "%s" is not supported in %s::$%s. You may use the `@OA\Property(type="")` annotation to specify it manually.', $types[0]->getBuiltinType(), $model->getType()->getClassName(), $propertyName));
+    }
+
+    /**
+     * Mark properties as required while ordering them in the same order as the properties of the schema.
+     * Then append the original required properties.
+     */
+    private function markRequiredProperties(OA\Schema $schema): void
+    {
+        if (Generator::isDefault($properties = $schema->properties)) {
+            return;
+        }
+
+        $newRequired = [];
+        foreach ($properties as $property) {
+            if (is_array($schema->required) && \in_array($property->property, $schema->required, true)) {
+                $newRequired[] = $property->property;
+                continue;
+            }
+
+            if (true === $property->nullable || !Generator::isDefault($property->default)) {
+                continue;
+            }
+            $newRequired[] = $property->property;
+        }
+
+        if ([] !== $newRequired) {
+            $originalRequired = Generator::isDefault($schema->required) ? [] : $schema->required;
+
+            $schema->required = array_values(array_unique(array_merge($newRequired, $originalRequired)));
+        }
     }
 
     public function supports(Model $model): bool

src/PropertyDescriber/RequiredPropertyDescriber.php
@@ -16,6 +16,8 @@ use OpenApi\Generator;
 
 /**
  * Mark a property as required if it is not nullable.
+ *
+ * @deprecated {@see ObjectModelDescriber::markRequiredProperties()}
  */
 final class RequiredPropertyDescriber implements PropertyDescriberInterface, PropertyDescriberAwareInterface
 {

tests/Functional/FunctionalTest.php
@@ -252,6 +252,9 @@ class FunctionalTest extends WebTestCase
                 ],
                 'schema' => 'User',
                 'required' => [
+                    'email',
+                    'location',
+                    'friendsNumber',
                     'creationDate',
                     'users',
                     'status',

tests/Functional/ValidationGroupsFunctionalTest.php
@@ -89,6 +89,7 @@ class ValidationGroupsFunctionalTest extends WebTestCase
             'required' => [
                 'property',
                 'propertyInDefaultGroup',
+                'propertyArray',
             ],
         ];
 

commit 748c6e0564688b6dcd0fdb5f73fd5497bbd471f8

revert: bring back schema to interface (#2360)
reverts potential breaking change introduced in
https://github.com/nelmio/NelmioApiDocBundle/pull/2359

phpstan-baseline.neon
@@ -36,6 +36,11 @@ parameters:
 			path: src/PropertyDescriber/PropertyDescriberInterface.php
 
 		-
+			message: "#^PHPDoc tag @param references unknown parameter\\: \\$schema$#"
+			count: 1
+			path: src/PropertyDescriber/PropertyDescriberInterface.php
+
+		-
 			message: "#^Method Nelmio\\\\ApiDocBundle\\\\PropertyDescriber\\\\PropertyDescriberInterface\\:\\:describe\\(\\) invoked with 5 parameters, 2\\-3 required\\.$#"
 			count: 1
 			path: src/PropertyDescriber/RequiredPropertyDescriber.php

src/PropertyDescriber/PropertyDescriberInterface.php
@@ -19,11 +19,12 @@ interface PropertyDescriberInterface
     /**
      * @param Type[]               $types
      * @param string[]|null        $groups  Deprecated use $context['groups'] instead
+     * @param Schema               $schema  Allows to make changes inside of the schema (e.g. adding required fields)
      * @param array<string, mixed> $context Context options for describing the property
      *
      * @return void
      */
-    public function describe(array $types, Schema $property, ?array $groups = null /* , array $context = [] */);
+    public function describe(array $types, Schema $property, ?array $groups = null /* , ?Schema $schema = null */ /* , array $context = [] */);
 
     /**
      * @param Type[] $types