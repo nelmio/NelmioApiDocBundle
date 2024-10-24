5f0bd80fc070387e29284986f03ac262f2678ad9
djordy <djordy.koert@yoursurprise.com>
2024-10-24 23:48:21 +0200

    Revert "remove api-platform 2 dummy"
    This reverts commit ccc9430222097710c8e14740417519082ebabb54.

---
+++ tests/Functional/EntityExcluded/ApiPlatform2/Dummy.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the NelmioApiDocBundle package.
+ *
+ * (c) Nelmio
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\ApiDocBundle\Tests\Functional\EntityExcluded\ApiPlatform2;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author Guilhem N. <egetick@gmail.com>
+ *
+ * @ApiResource(
+ *   collectionOperations={
+ *     "get"={"method"="GET"},
+ *     "custom2"={"path"="/foo", "method"="GET"},
+ *     "custom"={"path"="/foo", "method"="POST"},
+ *   },
+ *   itemOperations={"get"={"method"="GET"}})
+ * )
+ */
+class Dummy
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank
+     *
+     * @ApiProperty(iri="http://schema.org/name")
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
