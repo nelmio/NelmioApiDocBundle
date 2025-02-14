0b80953b72322328d4713bce7408b1da8a5b8211
djordy <djordy.koert@yoursurprise.com>
2025-02-14 16:59:49 +0100

    register RouteAttributeDescriber as service

--- src/DependencyInjection/NelmioApiDocExtension.php
+++ src/DependencyInjection/NelmioApiDocExtension.php
@@ -31,6 +31,7 @@ use Nelmio\ApiDocBundle\RouteDescriber\RouteArgumentDescriber\SymfonyMapQueryPar
 use Nelmio\ApiDocBundle\RouteDescriber\RouteArgumentDescriber\SymfonyMapQueryStringDescriber;
 use Nelmio\ApiDocBundle\RouteDescriber\RouteArgumentDescriber\SymfonyMapRequestPayloadDescriber;
 use Nelmio\ApiDocBundle\RouteDescriber\RouteArgumentDescriber\SymfonyMapUploadedFileDescriber;
+use Nelmio\ApiDocBundle\RouteDescriber\RouteAttributeDescriber;
 use Nelmio\ApiDocBundle\Routing\FilteredRouteCollectionBuilder;
 use OpenApi\Generator;
 use Symfony\Component\Config\FileLocator;
@@ -127,6 +128,13 @@ final class NelmioApiDocExtension extends Extension implements PrependExtensionI
                         $areaConfig['security'],
                     ])
                     ->addTag(sprintf('nelmio_api_doc.describer.%s', $area), ['priority' => -200]);
+
+                $container->register('nelmio_api_doc.route_describers.route_attribute', RouteAttributeDescriber::class)
+                    ->setPublic(false)
+                    ->setArguments([
+                        $areaConfig['security'],
+                    ])
+                    ->addTag('nelmio_api_doc.route_describer', ['priority' => -200]);
             }
 
             $container->register(\sprintf('nelmio_api_doc.describers.config.%s', $area), ExternalDocDescriber::class)
