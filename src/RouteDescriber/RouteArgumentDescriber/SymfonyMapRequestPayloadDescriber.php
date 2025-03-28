82492f418d10353c7d6b1aea96ec57b81b5e406d
djordy <djordy.koert@yoursurprise.com>
2025-03-28 21:47:38 +0100

    fix(#2462): MapRequestPayload with scalar array

--- src/RouteDescriber/RouteArgumentDescriber/SymfonyMapRequestPayloadDescriber.php
+++ src/RouteDescriber/RouteArgumentDescriber/SymfonyMapRequestPayloadDescriber.php
@@ -15,18 +15,28 @@ namespace Nelmio\ApiDocBundle\RouteDescriber\RouteArgumentDescriber;
 
 use Nelmio\ApiDocBundle\Describer\ModelRegistryAwareInterface;
 use Nelmio\ApiDocBundle\Describer\ModelRegistryAwareTrait;
-use Nelmio\ApiDocBundle\Model\Model;
+use Nelmio\ApiDocBundle\OpenApiPhp\Util;
+use Nelmio\ApiDocBundle\PropertyDescriber\PropertyDescriberInterface;
+use Nelmio\ApiDocBundle\TypeDescriber\TypeDescriberInterface;
 use OpenApi\Annotations as OA;
+use OpenApi\Generator;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
-use Symfony\Component\PropertyInfo\Type;
-use Symfony\Component\Validator\Constraints\GroupSequence;
+use Symfony\Component\PropertyInfo\Type as LegacyType;
+use Symfony\Component\TypeInfo\Type;
 
 final class SymfonyMapRequestPayloadDescriber implements RouteArgumentDescriberInterface, ModelRegistryAwareInterface
 {
     use ModelRegistryAwareTrait;
 
+    public function __construct(
+        private PropertyDescriberInterface|TypeDescriberInterface $propertyDescriber,
+    ) {
+    }
+
+    /** @deprecated */
     public const CONTEXT_ARGUMENT_METADATA = 'nelmio_api_doc_bundle.argument_metadata.'.self::class;
+    /** @deprecated */
     public const CONTEXT_MODEL_REF = 'nelmio_api_doc_bundle.model_ref.'.self::class;
 
     public function describe(ArgumentMetadata $argumentMetadata, OA\Operation $operation): void
@@ -35,40 +45,98 @@ final class SymfonyMapRequestPayloadDescriber implements RouteArgumentDescriberI
             return;
         }
 
-        $typeClass = $argumentMetadata->getType();
-
-        $reflectionAttribute = new \ReflectionClass(MapRequestPayload::class);
-        if (Type::BUILTIN_TYPE_ARRAY === $typeClass && $reflectionAttribute->hasProperty('type') && null !== $attribute->type) {
-            $typeClass = $attribute->type;
+        if (LegacyType::BUILTIN_TYPE_ARRAY === $argumentMetadata->getType() && property_exists($attribute, 'type')) {
+            $type = new LegacyType(
+                $argumentMetadata->getType(),
+                $argumentMetadata->isNullable(),
+                null,
+                true,
+                [new LegacyType(LegacyType::BUILTIN_TYPE_INT)],
+                [new LegacyType(
+                    class_exists($attribute->type) ? LegacyType::BUILTIN_TYPE_OBJECT : $attribute->type,
+                    false,
+                    class_exists($attribute->type) ? $attribute->type : null,
+                )]
+            );
+        } else {
+            $type = new LegacyType(
+                LegacyType::BUILTIN_TYPE_OBJECT,
+                $argumentMetadata->isNullable(),
+                $argumentMetadata->getType(),
+            );
         }
 
-        $modelRef = $this->modelRegistry->register(new Model(
-            new Type(Type::BUILTIN_TYPE_OBJECT, false, $typeClass),
-            groups: $this->getGroups($attribute),
-            serializationContext: $attribute->serializationContext,
-        ));
+        if ($this->propertyDescriber instanceof ModelRegistryAwareInterface) {
+            $this->propertyDescriber->setModelRegistry($this->modelRegistry);
+        }
 
-        $operation->_context->{self::CONTEXT_ARGUMENT_METADATA} = $argumentMetadata;
-        $operation->_context->{self::CONTEXT_MODEL_REF} = $modelRef;
+        /** @var OA\RequestBody $requestBody */
+        $requestBody = Util::getChild($operation, OA\RequestBody::class);
+        Util::modifyAnnotationValue($requestBody, 'required', !($argumentMetadata->hasDefaultValue() || $argumentMetadata->isNullable()));
+
+        $formats = $attribute->acceptFormat;
+        if (!\is_array($formats)) {
+            $formats = [$attribute->acceptFormat ?? 'json'];
+        }
+
+        foreach ($formats as $format) {
+            if (!Generator::isDefault($requestBody->content)) {
+                continue;
+            }
+
+            $contentSchema = $this->getContentSchemaForType($requestBody, $format);
+
+            if ($this->propertyDescriber instanceof TypeDescriberInterface) {
+                $types = self::toTypeInfoType($type);
+            } else {
+                $types = [$type];
+            }
+
+            if ($this->propertyDescriber->supports($types, $attribute->serializationContext)) {
+                $this->propertyDescriber->describe($types, $contentSchema, $attribute->serializationContext);
+
+                return;
+            }
+        }
     }
 
-    /**
-     * @return string[]|null
-     */
-    private function getGroups(MapRequestPayload $attribute): ?array
+    private function getContentSchemaForType(OA\RequestBody $requestBody, string $type): OA\Schema
     {
-        if (\is_string($attribute->validationGroups)) {
-            return [$attribute->validationGroups];
-        }
+        Util::modifyAnnotationValue($requestBody, 'content', []);
+        $contentType = match ($type) {
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            default => throw new \InvalidArgumentException('Unsupported media type'),
+        };
 
-        if (\is_array($attribute->validationGroups)) {
-            return $attribute->validationGroups;
-        }
+        $mediaType = Util::getCollectionItem($requestBody, OA\MediaType::class, [
+            'mediaType' => $contentType,
+        ]);
 
-        if ($attribute->validationGroups instanceof GroupSequence) {
-            return $attribute->validationGroups->groups;
-        }
+        return Util::getChild(
+            $mediaType,
+            OA\Schema::class
+        );
+    }
 
-        return null;
+    /** @see \Symfony\Component\PropertyInfo\Util\LegacyTypeConverter::toTypeInfoType() */
+    private static function toTypeInfoType(LegacyType $legacyType): Type
+    {
+        $typeInfoType = match ($legacyType->getBuiltinType()) {
+            LegacyType::BUILTIN_TYPE_BOOL => Type::bool(),
+            LegacyType::BUILTIN_TYPE_FALSE => Type::false(),
+            LegacyType::BUILTIN_TYPE_TRUE => Type::true(),
+            LegacyType::BUILTIN_TYPE_FLOAT => Type::float(),
+            LegacyType::BUILTIN_TYPE_INT => Type::int(),
+            LegacyType::BUILTIN_TYPE_STRING => Type::string(),
+            LegacyType::BUILTIN_TYPE_NULL => Type::null(),
+            LegacyType::BUILTIN_TYPE_ARRAY => Type::array(self::toTypeInfoType($legacyType->getCollectionValueTypes()), self::toTypeInfoType($legacyType->getCollectionKeyTypes())),
+            LegacyType::BUILTIN_TYPE_OBJECT => Type::object($legacyType->getClassName()),
+            default => throw new \InvalidArgumentException(\sprintf('"%s" is not a valid supported MapRequestPayload type.', $legacyType->getBuiltinType())),
+        };
+
+        return LegacyType::BUILTIN_TYPE_NULL === $legacyType->getBuiltinType() || $legacyType->isNullable()
+            ? Type::nullable($typeInfoType)
+            : $typeInfoType;
     }
 }
