f2f7f3cc68e6ee2994efd39a6b2cca5cd46a6164
djordy <djordy.koert@yoursurprise.com>
2025-05-24 15:27:16 +0200

    remove class_exists check for AbstractUid

--- src/DependencyInjection/NelmioApiDocExtension.php
+++ src/DependencyInjection/NelmioApiDocExtension.php
@@ -189,10 +189,6 @@ final class NelmioApiDocExtension extends Extension implements PrependExtensionI
         $container->registerForAutoconfiguration(ModelDescriberInterface::class)
             ->addTag('nelmio_api_doc.model_describer');
 
-        if (!class_exists(\Symfony\Component\Uid\AbstractUid::class)) {
-            $container->removeDefinition('nelmio_api_doc.object_model.property_describers.uuid');
-        }
-
         // Import services needed for each library
         $loader->load('php_doc.xml');
 
