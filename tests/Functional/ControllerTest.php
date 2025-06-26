1b1236d198ffbec797eb0d9b6ace78e253a1b0b7
djordy <djordy.koert@yoursurprise.com>
2025-06-26 16:20:33 +0200

    fix(#2509): test security

--- tests/Functional/ControllerTest.php
+++ tests/Functional/ControllerTest.php
@@ -579,7 +579,7 @@ final class ControllerTest extends WebTestCase
             'InvoiceDocumentController',
         ];
 
-        yield '' => [
+        yield 'Controller as service' => [
             null,
             'controller-as-service',
             [],
@@ -587,6 +587,35 @@ final class ControllerTest extends WebTestCase
             [
                 'web_custom_controller' => (new Definition(SecuredApiController::class))
                 ->setPublic(true),
+            ],
+            function (RoutingConfigurator $routes) {
+                $routes->add('route_name', '/')
+                    ->controller('web_custom_controller::fetchArticleAction')
+                    ->methods(['GET']);
+            },
+        ];
+
+        yield 'Controller as service with security' => [
+            null,
+            'controller-as-service-with-security',
+            [],
+            [
+                'nelmio_api_doc' => [
+                    'areas' => [
+                        'default' => [
+                            'security' => [
+                                'BasicAuth' => [
+                                    'type' => 'http',
+                                    'scheme' => 'basic',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                'web_custom_controller' => (new Definition(SecuredApiController::class))
+                    ->setPublic(true),
             ],
             function (RoutingConfigurator $routes) {
                 $routes->add('route_name', '/')
