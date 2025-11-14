b625aa6cba40d3125bcd2c3abb0c7361350bf6f8
Djordy Koert <djordy.koert@gmail.com>
2025-11-14 20:56:58 +0100

    fix(configuration): validate `type_info` option on Symfony 6 (#2615)
    ## Description
    
    Makes it impossible to enable `type_info` config option on Symfony 6.
    
    Closes https://github.com/nelmio/NelmioApiDocBundle/issues/2614
    
    ## What type of PR is this? (check all applicable)
    - [x] Bug Fix
    - [ ] Feature
    - [ ] Refactor
    - [ ] Deprecation
    - [ ] Breaking Change
    - [ ] Documentation Update
    - [ ] CI
    
    ## Checklist
    - [ ] I have made corresponding changes to the documentation (`docs/`)
    - [ ] I have made corresponding changes to the changelog
    (`CHANGELOG.md`)

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,6 +14,7 @@ namespace Nelmio\ApiDocBundle\DependencyInjection;
 use Nelmio\ApiDocBundle\Render\Html\AssetsMode;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 
 final class Configuration implements ConfigurationInterface
 {
@@ -28,6 +29,10 @@ final class Configuration implements ConfigurationInterface
                 ->booleanNode('type_info')
                     ->info('Use the symfony/type-info component for determining types.')
                     ->defaultFalse()
+                    ->validate()
+                        ->ifTrue(static fn ($v) => true === $v && !method_exists(PropertyInfoExtractor::class, 'getType'))
+                        ->thenInvalid('the type_info option requires Symfony 7 or higher. Please upgrade Symfony or set type_info to false.')
+                    ->end()
                 ->end()
                 ->booleanNode('use_validation_groups')
                     ->info('If true, `groups` passed to @Model annotations will be used to limit validation constraints')
